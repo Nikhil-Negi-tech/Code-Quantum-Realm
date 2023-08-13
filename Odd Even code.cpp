d277eb9ec05b775d0dd6e7d1abbe4dd192aa042e
NIKHIL NEGI <139095641+Nikhil-Negi-tech@users.noreply.github.com>
2023-08-13 21:03:25 +0530

    Add files via upload
    Code to Check Whether the integer is odd or Even

---
+++ Odd Even code.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    if(t%2==0)
+    {
+        cout<<"even"<<endl;
+    }
+    else
+    {
+        cout<<"odd"<<endl;
+    }
+    return 0;
+}
